4e8f88cbda7865aafc8c9e974d83bb41de465bab
Risto Turtiainen <risto.turtiainen@cybercom.com>
2016-05-12 11:51:44 +0300

    init commit

---
+++ src/main/java/fi/vm/sade/ploteus/App.java
@@ -0,0 +1,18 @@
+package fi.vm.sade.ploteus;
+
+import fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO;
+import fi.vm.sade.tarjonta.service.resources.v1.dto.KoulutusHakutulosV1RDTO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        HakukohdeDTO h = new HakukohdeDTO();
+        KoulutusHakutulosV1RDTO kh = new KoulutusHakutulosV1RDTO();
+    }
+}
