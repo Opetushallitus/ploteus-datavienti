7b7e6875c1257ba4973ba97ac07bc05f2742088f
Kami Nasri <kami.nasri@cybercom.com>
2016-05-16 10:05:30 +0300

    Created Koulutus model

---
+++ src/main/java/fi/vm/sade/model/Koulutus.java
@@ -0,0 +1,189 @@
+package fi.vm.sade.model;
+
+public class Koulutus {
+	private String oid;
+	private String countryCode;
+	private String title;
+	private String description;
+	private String url;
+	private String educationLevel;
+	
+	private String thematicAreas2k13; // Check if this is correct
+	
+	private String infoLang;
+	private String teachingLang;
+	private String studyType;
+	private String durationInfo;
+	private String startDate;
+	private String qualificationAwarded;
+	private String qualAwardDesc;
+	private String awardingBody;
+	private String cost;
+	private String credits;
+	private String providerName;
+	private String courseAddress;
+	private String specialArrangements;
+	
+	public String getOid() {
+		return oid;
+	}
+	
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getEducationLevel() {
+		return educationLevel;
+	}
+	
+	public void setEducationLevel(String educationLevel) {
+		this.educationLevel = educationLevel;
+	}
+	
+	public String getThematicAreas2k13() {
+		return thematicAreas2k13;
+	}
+	
+	public void setThematicAreas2k13(String thematicAreas2k13) {
+		this.thematicAreas2k13 = thematicAreas2k13;
+	}
+	
+	public String getInfoLang() {
+		return infoLang;
+	}
+	
+	public void setInfoLang(String infoLang) {
+		this.infoLang = infoLang;
+	}
+	
+	public String getTeachingLang() {
+		return teachingLang;
+	}
+	
+	public void setTeachingLang(String teachingLang) {
+		this.teachingLang = teachingLang;
+	}
+	
+	public String getStudyType() {
+		return studyType;
+	}
+	
+	public void setStudyType(String studyType) {
+		this.studyType = studyType;
+	}
+	
+	public String getDurationInfo() {
+		return durationInfo;
+	}
+	
+	public void setDurationInfo(String durationInfo) {
+		this.durationInfo = durationInfo;
+	}
+	
+	public String getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	
+	public String getQualificationAwarded() {
+		return qualificationAwarded;
+	}
+	
+	public void setQualificationAwarded(String qualificationAwarded) {
+		this.qualificationAwarded = qualificationAwarded;
+	}
+	
+	public String getQualAwardDesc() {
+		return qualAwardDesc;
+	}
+	
+	public void setQualAwardDesc(String qualAwardDesc) {
+		this.qualAwardDesc = qualAwardDesc;
+	}
+	
+	public String getAwardingBody() {
+		return awardingBody;
+	}
+	
+	public void setAwardingBody(String awardingBody) {
+		this.awardingBody = awardingBody;
+	}
+	
+	public String getCost() {
+		return cost;
+	}
+	
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+	
+	public String getCredits() {
+		return credits;
+	}
+	
+	public void setCredits(String credits) {
+		this.credits = credits;
+	}
+	
+	public String getProviderName() {
+		return providerName;
+	}
+	
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+	
+	public String getCourseAddress() {
+		return courseAddress;
+	}
+	
+	public void setCourseAddress(String courseAddress) {
+		this.courseAddress = courseAddress;
+	}
+	
+	public String getSpecialArrangements() {
+		return specialArrangements;
+	}
+	
+	public void setSpecialArrangements(String specialArrangements) {
+		this.specialArrangements = specialArrangements;
+	}
+
+	
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	
+	
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+}
