746f48cd4775995c4dc55c14cc87beb0435decbb
Jussi Vesala <jussi.vesala@reaktor.fi>
2016-06-17 13:14:26 +0300

    add logging skeleton

--- src/main/java/fi/vm/sade/ploteus/App.java
+++ src/main/java/fi/vm/sade/ploteus/App.java
@@ -2,6 +2,8 @@ package fi.vm.sade.ploteus;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,13 +16,16 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath*:applicationContext.xml"})
 @ComponentScan("fi.vm.sade")
 public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(App.class, args);
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        log.info("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
+
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            log.info(beanName);
         }
     }
 }
